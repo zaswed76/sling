044c6e165a0d1957697ec4d2d7ca4c497267104b
Сергей <zaswed76@gmail.com>
2020-04-13 14:29:27 +0300

    create

--- lingvo/gui/custom/drag.py
+++ lingvo/gui/custom/drag.py
@@ -13,7 +13,7 @@ class Widget(QPushButton):
 class DragLabel(QLabel):
     def __init__(self, *__args):
         super().__init__(*__args)
-        self.setFont(QFont("arial", 30))
+        self.setFont(QFont("Helvetica", 30))
         self.setAlignment(Qt.AlignLeft)
 
     def mouseMoveEvent(self, e):
@@ -24,7 +24,7 @@ class DragLabel(QLabel):
         drag.setHotSpot(e.pos() - self.rect().topLeft())
         dropAction = drag.exec_(Qt.MoveAction)
 
-class DelBtn(QPushButton):
+class ControlLabelBtn(QPushButton):
     def __init__(self, icon, text,  parent, *__args):
         super().__init__(*__args)
         self.setParent(parent)
@@ -32,21 +32,36 @@ class DelBtn(QPushButton):
         self.lbText = text
         self.setFixedSize(25,25)
         self.setFont(QFont("arial", 12))
-        self.setText("x")
+        # self.setText("x")
 
+class DropLabelControl(QFrame):
+    def __init__(self, *__args):
+        super().__init__(*__args)
+        self.box = QHBoxLayout(self)
+        self.box.setSpacing(4)
+        self.box.setContentsMargins(0, 0, 0, 0)
+
+        self.delBtn = ControlLabelBtn("", self.parent().text(), self)
+        self.delBtn.setObjectName("delBtn")
+        self.delBtn.clicked.connect(self.parent().parent().delLabel)
+
+        self.tuneBtn = ControlLabelBtn("", self.parent().text(), self)
+        self.tuneBtn.setObjectName("tuneBtn")
+        self.tuneBtn.clicked.connect(self.parent().parent().tuneLabel)
+        self.box.addWidget(self.delBtn)
+        self.box.addWidget(self.tuneBtn)
 
 class DropLabel(QLabel):
     def __init__(self, *__args):
         super().__init__(*__args)
-        # self.setFont(QFont("arial", 160))
+
         self.setAlignment(Qt.AlignCenter)
         self.installEventFilter(self)
         self.setStyleSheet("QLabel { color: #2D2D2D }")
 
+        self.dropLabelControl = DropLabelControl(self)
 
 
-        self.btn = DelBtn("", self.text(), self)
-        self.btn.clicked.connect(self.parent().delLabel)
         text, self.suffix = self.text().split("_")
         self.setText(text)
         self._tuneText()
@@ -54,40 +69,40 @@ class DropLabel(QLabel):
     def _tuneText(self):
         if self.text() == "Пример":
             self.setText("this is an example in english")
-            self.setFont(QFont("arial", 14, italic=True))
+            self.setFont(QFont("Helvetica", 14, italic=True))
             self.setStyleSheet("QLabel { color: #144676 }")
             self.setAlignment(Qt.AlignLeft)
             self.setContentsMargins(50, 10, 0, 0)
 
         elif self.text() == "Word":
-            self.setFont(QFont("arial", 56))
+            self.setFont(QFont("Helvetical", 56))
             self.setAlignment(Qt.AlignCenter)
 
         elif self.text() == "Транскрипция":
             self.setText("[Транскрипция]")
-            self.setFont(QFont("arial", 30))
+            self.setStyleSheet("QLabel { color: #6E6E6E }")
+            self.setFont(QFont("Helvetica", 30))
             self.setAlignment(Qt.AlignCenter)
 
         elif self.text() == "Перевод":
-            self.setFont(QFont("arial", 56))
+            self.setFont(QFont("Helvetica", 56))
             self.setAlignment(Qt.AlignCenter)
 
 
 
     def eventFilter(self, obj, event):
         if event.type() == 11:  # Если мышь покинула область фиджета
-            self.btn.hide()  # выполнить  callback1()
+            self.dropLabelControl.hide()  # выполнить  callback1()
         elif event.type() == 10:  # Если мышь над виджетом
-            self.btn.show()  # выполнить  callback2()
-
-
+            self.dropLabelControl.show()  # выполнить  callback2()
         return False
 
 class DragFrame(QFrame):
     def __init__(self):
         super().__init__()
         self.box = QVBoxLayout(self)
-        self.setStyleSheet('background-color:: #d9d9d9;')
+        self.setStyleSheet('QFrame{background-color: #D1D1D3;}')
+
 
         self.setAcceptDrops(True)
         self.labels = {}
@@ -113,3 +128,8 @@ class DragFrame(QFrame):
         key = lb.lbText
         self.box.removeWidget(self.labels[key])
         self.labels[key].deleteLater()
+
+    def tuneLabel(self):
+        lb = self.sender()
+        key = lb.lbText
+        print(key)
