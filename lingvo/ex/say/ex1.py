13bbe816ecafc4aeb10ec73ced2e879612be50f7
zaswed76 <zaswed76@gmail.com>
2020-04-19 00:22:50 +0300

    create doc

--- lingvo/ex/say/ex1.py
+++ lingvo/ex/say/ex1.py
@@ -1,4 +1,5 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say('dog')
-engine.runAndWait()
+
+from gtts import gTTS
+for i in range(10):
+    tts = gTTS('This my cat', lang='en')
+    tts.save('tts_output{}.mp3'.format(str(i)))
