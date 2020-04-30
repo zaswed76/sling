44974d9dab8ccbc9ea1203ddb6af5f0aeab98f90
Сергей <zaswed76@gmail.com>
2020-04-30 20:54:06 +0300

    create

--- lingvo2/core/soundloader.py
+++ lingvo2/core/soundloader.py
@@ -1,5 +1,29 @@
+from gtts import gTTS
+from pathlib import Path
+
+def soundLoader(wordList, dirForSounds):
+    print(dirForSounds, type(dirForSounds))
+
+    for line in wordList:
+        tts = loadGtts(line)
+        file_name = Path(dirForSounds) / "{}.mp3".format(line)
+        saveTTS(tts, file_name)
 
 
-def soundLoader(wordList):
-    wordList = ["cat", "dog"]
-    print(wordList)
+def loadGtts(line):
+    return gTTS(line, lang='en')
+
+def saveTTS(gtts, path):
+    gtts.save(path)
+
+
+if __name__ == '__main__':
+    import paths
+    datadir = paths.DATA / "family"
+    Path(datadir / "sounds").mkdir(parents=True, exist_ok=True)
+    targetDir = datadir / "sounds"
+
+    wordList = ["mother", "father"]
+    soundLoader(wordList, targetDir)
+
+
